e3b2da2b8c0014c6bc37162f2a28547c8e606b1d
Arthur-Miertschink <arthursm50@gmail.com>
2021-04-20 19:37:51 -0300

    Projeto Series

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Series.Interfaces;
+using System;
+
+
+namespace Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Series>
+
+    {
+        private List<Series> listaSerie = new List<Series>();
+
+        public void Atualiza(int id, Series entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Series objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Series> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Series RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+        
+
+
+    }
+}
